9aa71154a39d47f6ef74950f89b0ecaeddbd1307
Danny O'Brien <dannyobrien761@gmail.com>
2024-07-18 13:23:24 +0000

    js code for 2 body parts and 1  card

--- assets/script.js
+++
@@ -1,3 +0,0 @@
-const herbImg = document.getElementById('card-header')
-const title = document.getElementById('herb-title')
-const benifits = document.getElementById('herb-benifits')
---
+++ script.js
@@ -0,0 +1,37 @@
+const herbImg = document.querySelector('.card-header');
+const title = document.getElementById('herb-title');
+const benifits = document.getElementById('herb-benifits');
+const eyeBox = document.getElementById('eye');
+const hairBox = document.getElementById('hair');
+
+
+eyeBox.addEventListener('click', () => getHerb('eyeHerb'));
+hairBox.addEventListener('click', () => getHerb('hairHerb'));
+
+
+function getEyeCard(){
+    herbImg.innerHTML = 
+    '<img src="https://unsplash.com/photos/selective-focus-photography-of-blue-petaled-flowers-in-clear-glass-jar-Tinbs_bjKxA" alt = "">';
+    title.innerHTML = 'Eye bright';
+    benifits.innerHTML = 'Clears mucus in the tear ducts';
+
+}
+
+function getHairCard(){
+    herbImg.innerHTML = 
+    '<img src="https://unsplash.com/photos/green-leafed-plants-on-white-background-INjdgkCwHp0" alt = "">';
+    title.innerHTML = 'Rosemary';
+    benifits.innerHTML = 'Cleans and strengthens hair';
+
+}
+
+
+const getHerb = (boxChoice) => {
+
+    if (boxChoice === 'eyeHerb' ) {
+
+        getEyeCard();
+    } else if(boxChoice === 'hairHerb'){
+        getHairCard();
+    }
+};
