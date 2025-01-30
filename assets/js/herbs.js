854cc554022f2688a2bb299e23894bf9e83e05a3
Danny O'Brien <dannyobrien761@gmail.com>
2025-01-30 11:42:53 +0000

    changed herbData to herbs.js reformatted herbData
    reduced duplicate code in script.js, changed to smaller functions to call array of objects to category key
    
    updated index.html to replace hardcoded herb cards to simple card container

---
+++ assets/js/herbs.js
@@ -0,0 +1,55 @@
+
+  // herbs.js
+export const herbData = {
+  eyeHerbs: [
+    { title: "Eyebright", benefits: "Clears mucus in the tear ducts", imageUrl: "https://images.unsplash.com/photo-1504382103100-db7e92322d39?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Chamomile", benefits: "Reduces eye inflammation", imageUrl: "https://plus.unsplash.com/premium_photo-1675441003177-1dbcf11fa772?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Bilberry", benefits: "Improves night vision", imageUrl: "https://images.unsplash.com/photo-1658509565784-d425ada6c327?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  hairHerbs: [
+    { title: "Rosemary", benefits: "Cleans and strengthens hair", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Lavender", benefits: "Promotes hair growth", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Peppermint", benefits: "Stimulates scalp", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  brainHerbs: [
+    { title: "Skullcap", benefits: "Cleans and strengthens hair", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Resveratrol", benefits: "Antioxidant and anti-inflammatory properties to protect you against diseases like cancer, diabetes, and Alzheimer's disease", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Green-Tea", benefits: "Reduces inflammation after head injury", imageUrl: "https://images.unsplash.com/photo-1701520839071-55bdfe64c5ed?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Fasting", benefits: "Clears excess mucus accumulation, increases clarity, and reduces brain fog", imageUrl: "https://images.unsplash.com/photo-1559839914-17aae19cec71?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Rosemary", benefits: "Stimulates scalp, improving circulation", imageUrl: "https://plus.unsplash.com/premium_photo-1661697466676-200ffe1cbf1e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Cayenne and Ginger", benefits: "Sends blood to the head, improving circulation and clearing brain fog", imageUrl: "https://images.unsplash.com/photo-1599940859674-a7fef05b94ae?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  teethHerbs: [
+    { title: "Echinacea", benefits: "Anti-infective herbs, regulate bacteria, eliminating obvious infections and relieving pain.", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Nettle", benefits: "Addresses systemic mineral deficiencies particularly iron", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Sage", benefits: "Anti-infective, astringent, and circulation-stimulating", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  heartHerbs: [
+    { title: "Dandelion", benefits: "Cleans and strengthens hair", imageUrl: "https://images.unsplash.com/photo-1475140604894-b4aaba075542?q=80&w=1423&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Resveratrol", benefits: "Promotes hair growth", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Green-Tea", benefits: "Stimulates scalp", imageUrl: "https://images.unsplash.com/photo-1701520839071-55bdfe64c5ed?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  lungHerbs: [
+    { title: "Mugwort", benefits: "Cleans lungs", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Mullein Leaf", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Eucalyptus", benefits: "Relieves congestion", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  digestionHerbs: [
+    { title: "Seeds", benefits: "Cleans lungs", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Irish Moss", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Dillsk", benefits: "Relieves congestion", imageUrl: "https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wx" }
+  ],
+  lymphaticHerbs: [
+    { title: "Polk Root", benefits: "Cleans lungs", imageUrl: "https://plus.unsplash.com/premium_photo-1664200731344-ef1ad87aa0a6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Wormwood", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://plus.unsplash.com/premium_photo-1678653651270-9f0f68119d99?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Yoga", benefits: "Relieves congestion", imageUrl: "https://images.unsplash.com/photo-1447452001602-7090c7ab2db3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+  
+  redBloodCellHerbs: [
+    { title: "Nettle", benefits: "Cleans lungs", imageUrl: "https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Nettle Seed", benefits: "Antiviral and Antibacterial properties", imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6" },
+    { title: "Black Walnut", benefits: "Removes parasites", imageUrl: "https://plus.unsplash.com/premium_photo-1675720902465-82781aaa96bb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Pumpkin Seeds", benefits: "Fibre and helps remove parasites", imageUrl: "https://plus.unsplash.com/premium_photo-1674347954785-7604a0220776?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+  ],
+
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,404 +1,44 @@
+// script.js
+import { herbData } from './herbs.js'; // Importing the herb data
 
-// Since the order of the elements in the DOM is being used, the  DOMContentLoaded event to ensure it runs after the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-
-
-// Function to update herb cards with given herb information
-function updateHerbCards(herbs) {
-    console.log('Updating herb cards', herbs);
-    const cardsContainer = document.querySelector('.cards');
-    let herbCards = document.querySelectorAll('.herb-card');
-
-    // If there are not enough herb-card elements, create new ones
-    if (herbCards.length < herbs.length) {
-        for (let i = herbCards.length; i < herbs.length; i++) {
-            const newCard = document.createElement('div');
-            newCard.classList.add('herb-card');
-
-            const header = document.createElement('div');
-            header.classList.add('card-header');
-            header.innerHTML = '&nbsp;';
-
-            const content = document.createElement('div');
-            content.classList.add('card-content');
-
-            const title = document.createElement('h3');
-            title.classList.add('herb-title');
-            title.innerHTML = '&nbsp;';
-
-            const benefits = document.createElement('p');
-            benefits.classList.add('herb-benefits');
-            benefits.innerHTML = '&nbsp;';
-
-            const link = document.createElement('a');
-            link.classList.add('herb-link');
-            link.href = '#';
-            link.target = '_blank';
-            link.innerHTML = '&nbsp;';
-
-            content.appendChild(title);
-            content.appendChild(benefits);
-            content.appendChild(link);
-            newCard.appendChild(header);
-            newCard.appendChild(content);
-
-            cardsContainer.appendChild(newCard);
-        }
-        // Re-select the herbCards after adding new ones
-        herbCards = document.querySelectorAll('.herb-card');
-    }
-
- 
-    herbCards.forEach((card, index) => {
-        const header = card.querySelector('.card-header');
-        const title = card.querySelector('.herb-title');
-        const benefits = card.querySelector('.herb-benefits');
-        const link = card.querySelector('.herb-link');
-
-        if (header && title && benefits && herbs[index]) {
-            header.innerHTML = `<img src="${herbs[index].imageUrl}" alt="Herb image">`;
-            title.innerHTML = herbs[index].title;
-            benefits.innerHTML = herbs[index].benefits;
-            link.href = herbs[index].url;
-            link.innerHTML = 'Shop here';
-
-
-        } else {
-           // Log which elements are missing if there's an issue
-           console.error('Missing element(s) for herb card index:', index);
-           console.log('header:', header);
-           console.log('title:', title);
-           console.log('benefits:', benefits);
-           console.log('herbs[index]:', herbs[index]);
-
-          // Remove the card if there's no corresponding herb or element is missing
-            card.remove();  
-
-        }
-    });
-}
-
-// Attaching event listeners to boxes
+// Adding event listeners to body part selection
 document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('click', () => {
-        let herbType;
-        console.log('Box clicked:', box.id);
-        switch (box.id) {
-            case 'eye':
-                herbType = 'eyeHerb';
-                break;
-            case 'hair':
-                herbType = 'hairHerb';
-                break;
-            case 'brain':
-                herbType = 'brainHerb';
-                break;
-            case 'teeth':
-                herbType = 'teethHerb';
-                break;
-            case 'heart':
-                herbType = 'heartHerb';
-                break;
-            case 'lung':
-                herbType = 'lungHerb';
-                break;
-            case 'digestion-system':
-                herbType = 'digestionHerb';
-                break;
-            case 'lymphatic-system':
-                herbType = 'lymphaticHerb';
-                break;
-            case 'red-blood-cell':
-                herbType = 'redBloodCellHerb';
-                break;
-            default:
-                return;
-        }
-        getHerb(herbType);
-        console.log(herbType);
+        const category = box.id;  // Get the category based on clicked body part
+        const herbs = getHerbsByCategory(category);
+        displayHerbs(herbs); // Display the herbs related to the body part
     });
 });
 
+// Function to get herbs based on the category selected
+function getHerbsByCategory(category) {
+    const categoryData = herbData[category + 'Herbs']; // Access the correct category in the herbData object
+    return categoryData ? categoryData : []; // Return the herb data or an empty array if not found
+}
 
-//another event listener for box elements to highlight once selected
-// code snippet taken from expanding cards project from brad traversy and referenced in ReadMe
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-    box.addEventListener('click', () =>{
-        removeActiveClasses();
-        box.classList.add('active');
+// Function to display the herbs in the UI
+function displayHerbs(herbs) {
+    const contentContainer = document.querySelector('.cards');
+    contentContainer.innerHTML = ''; // Clear existing content
+    herbs.forEach(herb => {
+        const herbCard = createHerbCard(herb);
+        contentContainer.appendChild(herbCard); // Append each herb card to the container
     });
-})
-
-function removeActiveClasses() {
-    boxes.forEach(box => {
-        box.classList.remove('active');
-    });
-};
-
-//all get herb functions:
-
-
-// Function to get information for eye-related herbs
-function getEyeHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1504382103100-db7e92322d39?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Eyebright',
-            benefits: 'Clears mucus in the tear ducts'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1675441003177-1dbcf11fa772?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Chamomile',
-            benefits: 'Reduces eye inflammation'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1658509565784-d425ada6c327?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Bilberry',
-            benefits: 'Improves night vision'
-        }
-        
-    ];
-}
-// Function to get information for hair-related herbs
-function getHairHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Rosemary',
-            benefits: 'Cleans and strengthens hair'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Lavender',
-            benefits: 'Promotes hair growth'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Peppermint',
-            benefits: 'Stimulates scalp'
-        }
-        
-    ];
-
-}
-// Function to get information for brain-related herbs
-function getBrainHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Skullcap',
-            benefits: 'Cleans and strengthens hair',
-            url: 'https://www.amazon.co.uk/'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Resveratrol',
-            benefits: "antioxidant and anti-inflammatory properties to protect you against diseases like cancer, diabetes, and Alzheimer's disease",
-            url: 'https://www.amazon.co.uk/'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1701520839071-55bdfe64c5ed?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Green-Tea',
-            benefits: 'reduces inflamation, after head injury',
-            url: 'https://www.amazon.co.uk/'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1559839914-17aae19cec71?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Fasting',
-            benefits: 'Clear excess mucus accumilation, increases clarity and reduces brain fog',
-            url: 'https://www.amazon.co.uk/'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1661697466676-200ffe1cbf1e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Rosemary',
-            benefits: 'Stimulates scalp, improving circulation',
-            url: 'https://www.amazon.co.uk/'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1599940859674-a7fef05b94ae?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Cayenne and Ginger',
-            benefits: 'Sends Blood to the head, improving circulation and clearing brain fog',
-            url: 'https://www.amazon.co.uk/'
-        }
-    ];
-
 }
 
-// Function to get information for teeth-related herbs
-function getTeethHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Echinacea',
-            benefits: 'anti-infective herbs, regulate bacteria, eliminating obvious infections and relieving pain.'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Nettle',
-            benefits: 'to address systemic mineral deficiencies particularly iron'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Sage',
-            benefits: 'anti-infective, astringent, and circulation-stimulating'
-        }
-        
-    ];
-
+// Function to create a herb card element
+function createHerbCard(herb) {
+    const card = document.createElement('div');
+    card.classList.add('herb-card');
+    card.innerHTML = `
+        <div class="card-header">
+            <img src="${herb.imageUrl}" alt="${herb.title}">
+        </div>
+        <div class="card-content">
+            <h3 class="herb-title">${herb.title}</h3>
+            <p class="herb-benefits">${herb.benefits}</p>
+            <a class="herb-link" href="#" target="_blank">Learn more</a>
+        </div>
+    `;
+    return card;
 }
-// Function to get information for Heart-related herbs
-function getHeartHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1475140604894-b4aaba075542?q=80&w=1423&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Dandelion',
-            benefits: 'Cleans and strengthens hair'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Resveratrol',
-            benefits: 'Promotes hair growth'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1701520839071-55bdfe64c5ed?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Green-Tea',
-            benefits: 'Stimulates scalp'
-        }
-        
-    ];
-
-}
-// Function to get information for lung-related herbs
-function getLungHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Mugwort',
-            benefits: 'Cleans lungs'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Mullein Leaf',
-            benefits: 'Antiviral and Antibacterial properties'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Eucalyptus',
-            benefits: 'relieve congestion'
-        }
-        
-    ];
-
-}
-// Function to get information for lung-related herbs
-function getDigestionHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'seeds',
-            benefits: 'Cleans lungs'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'Irish Moss',
-            benefits: 'Antiviral and Antibacterial properties'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1673264303561-de2ab31df03c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'dillsk',
-            benefits: 'relieve congestion'
-        }
-        
-    ];
-
-}
-// Function to get information for lung-related herbs
-function getLymphaticHerbs() {
-    return [
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1664200731344-ef1ad87aa0a6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'polk root',
-            benefits: 'Cleans lungs'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1678653651270-9f0f68119d99?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'wormwood',
-            benefits: 'Antiviral and Antibacterial properties'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1447452001602-7090c7ab2db3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'yoga',
-            benefits: 'relieve congestion'
-        }
-        
-    ];
-
-}
-// Function to get information for lung-related herbs
-function getRedBloodCellHerbs() {
-    return [
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1509210459313-17feefdff5cd?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'nettle',
-            benefits: 'Cleans lungs'
-        },
-        {
-            imageUrl: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6',
-            title: 'nettle seed',
-            benefits: 'Antiviral and Antibacterial properties'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1675720902465-82781aaa96bb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Blackwalnut',
-            benefits: 'removes parasites'
-        },
-        {
-            imageUrl: 'https://plus.unsplash.com/premium_photo-1674347954785-7604a0220776?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            title: 'Pumkin Seeds',
-            benefits: 'Fibre and helps remove parasites'
-        }
-    ];
-
-}
-
-
-// Main function to get herb information based on the clicked box
-const getHerb = (boxChoice) => {
-    let herbs;
-    if (boxChoice === 'eyeHerb') {
-        herbs = getEyeHerbs();
-    } else if (boxChoice === 'hairHerb') {
-        herbs = getHairHerbs();
-    }
-    else if (boxChoice === 'brainHerb') {
-        herbs = getBrainHerbs();
-    }
-    else if (boxChoice === 'teethHerb') {
-        herbs = getTeethHerbs();
-    }
-    else if (boxChoice === 'heartHerb') {
-        herbs = getHeartHerbs();
-    }
-    else if (boxChoice === 'lungHerb') {
-        herbs = getLungHerbs();
-    }
-    else if (boxChoice === 'digestionHerb') {
-        herbs = getDigestionHerbs();
-    }
-    else if (boxChoice === 'lymphaticHerb') {
-        herbs = getLymphaticHerbs();
-    }
-    else if (boxChoice === 'redBloodCellHerb') {
-        herbs = getRedBloodCellHerbs();
-    }
-    updateHerbCards(herbs);
-    console.log(herbs);
-};
-
-
-
-
-//dom content loading cotainer end
-});
